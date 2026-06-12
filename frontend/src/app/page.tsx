3dbc57988adfef033e7fd4b4517e339777787976
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-06-12 20:16:38 +0200

    style(frontend): make layout responsive using flexbox and optimize SCSS with mixins

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import RateCard from '../../components/RateCard/RateCard';
+import Form from '../../components/Form/Form';
 
 export default function Home() {
 	return (
 		<main>
 			<RateCard />
+			<Form />
 		</main>
 	);
 }
